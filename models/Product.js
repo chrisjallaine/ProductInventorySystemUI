f07176e47d58de92a671388ef1b65964c5d07f74
NicoleMenorias <menorias.nicole@gmail.com>
2025-05-03 23:05:15 +0800

    product done

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  sku: { type: String, required: true, unique: true },
   description: { type: String },
   price: { type: Number, required: true, min: 0 },
   category_id: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
