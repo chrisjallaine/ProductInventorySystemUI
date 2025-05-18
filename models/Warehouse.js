1d25e8ddb88963e548e2f6a60df24c00b66c969e
kardcy <karylle.dc30@gmail.com>
2025-05-18 21:36:51 +0800

    added name in warehouse model

--- models/Warehouse.js
+++ models/Warehouse.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const warehouseSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   location: { type: String, required: true },
   capacity: { type: Number, required: true },
   currentUsage: { type: Number, default: 0 },
